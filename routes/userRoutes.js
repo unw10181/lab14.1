cb587f7092e00e382e2da6e2d320f197f8139296
unw10181 <uchennanwilliams@gmail.com>
2026-01-21 08:23:18 -0500

    login endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -34,3 +34,45 @@ router.post("/register", async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 });
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Incorrect email or password." });
+    }
+
+    // Check password
+    const isValid = await user.isCorrectPassword(password);
+    if (!isValid) {
+      return res.status(400).json({ message: "Incorrect email or password." });
+    }
+
+    // Create JWT
+    const token = jwt.sign(
+      {
+        _id: user._id,
+        username: user.username,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
+
+    res.json({
+      token,
+      user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+module.exports = router;
